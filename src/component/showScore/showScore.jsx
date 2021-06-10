ef74374613903c48b8877696a10f829d414fab0f
Andrey Khaylov <andrey.haylovv@yandex.ru>
2021-06-11 01:13:31 +0300

    [refactoring](showScore.jsx): правка кода

--- src/component/showScore/showScore.jsx
+++ src/component/showScore/showScore.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import s from './showScore.module.css'
-import Details from '../details/details'
-import Button from '../../assets/button/button'
+import s from './showScore.module.css';
+import Details from '../details/details';
+import Button from '../../assets/button/button';
 
 
 function ShowScore({ 
@@ -9,7 +9,7 @@ function ShowScore({
     currentAnswer, setCurrentAnswer,
     setShowScore, setCurrentQuestion,
 }) {
-    const [isShowDetails, setShowDetails] = useState(false)
+    const [isShowDetails, setShowDetails] = useState(false);
 
     function handleDetails() { // Выводится подробная таблица результата теста
         setShowDetails(true);
@@ -24,36 +24,34 @@ function ShowScore({
     }
     
     return (
-        <div>
-            {
-                !isShowDetails
-                ?
-                    <section className={s.score}>
-                        <h2 className={s.score__title}>Правильных ответов:
-                            <span className={
-                                (score === questionsBase.length && s.score__full) // Если все правильные ответы, число подсвечивается зеленым
-                                || (score ? s.score__count : s.score__null)       // Если нет правильных ответов - красным
-                            }
-                            >{score}
-                            </span>/ 
-                            <span className={s.score__full}>{questionsBase.length}</span>
-                        </h2>
-                        <Button handleClick={handleDetails}>
-                            Подробнее
-                        </Button>
-                        <Button handleClick={handleRestart}>
-                            Начать заново
-                        </Button>
-                    </section>
+        <>
+            {!isShowDetails ?
+                <section className={s.score}>
+                    <h2 className={s.score__title}>Правильных ответов:
+                        <span className={
+                            (score === questionsBase.length && s.score__full) // Если все правильные ответы, число подсвечивается зеленым
+                            || (score ? s.score__count : s.score__null) }     // Если нет правильных ответов - красным
+                        >{score}
+                        </span>/ 
+                        <span className={s.score__full}>
+                            {questionsBase.length}
+                        </span>
+                    </h2>
+                    <Button handleClick={handleDetails}>
+                        Подробнее
+                    </Button>
+                    <Button handleClick={handleRestart}>
+                        Начать заново
+                    </Button>
+                </section>
                 : 
-                    <Details score={score}
-                            currentAnswer={currentAnswer}
-                            questionsBase={questionsBase}
-                            setShowDetails={setShowDetails}
-                    />
+                <Details score={score}
+                        currentAnswer={currentAnswer}
+                        questionsBase={questionsBase}
+                        setShowDetails={setShowDetails}
+                />
             }
-        </div>
-        
+        </>
     );
 }
 
